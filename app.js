cce9ff6ae7e4538bfdabe189d72fadb9f095b32d
Ross Gonsolvis <gonsolvis@gmail.com>
2023-02-19 22:20:43 +0100

    iteration two

--- app.js
+++ app.js
@@ -16,17 +16,21 @@ const hbs = require('hbs');
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+require("./config")(app);
+require("./config/session.config")(app);
 
 // default value for title local
-const projectName = 'lab-express-basic-auth';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
-
+const projectName = 'lab-express-basic-auth';
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+const userRoutes = require('./routes/user.routes');
+app.use('/user', userRoutes);
+
+
+//THE ABOVE IS DIFFERENT ON MARIONAS VERSION
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
---
+++ routes/user.routes.js
@@ -0,0 +1,101 @@
+// const express = require('express');
+const router = require("express").Router();
+
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+
+const User = require("../models/User.model");
+
+// const isLoggedOut = require("../middleware/isLoggedOut");
+// const isLoggedIn = require("../middleware/isLoggedIn");
+
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Home page" })
+})
+
+router.get("/register",  (req, res, next) => {
+  res.render("users/register");
+});
+
+router.post("/register", (req, res, next) => {
+  let { username, password, passwordRepeat } = req.body;
+  if (username == "" || password == "" || passwordRepeat == "") {
+    res.render("users/register", { messageError: "There are missing fields" });
+    return;
+  }
+  else if (password != passwordRepeat) {
+    res.render("users/register", { messageError: "The passwords do not match" });
+    return;
+  }
+
+  User.find({ username })
+    .then(results => {
+      console.log("results ", results);
+      if (results.length != 0) {
+        //error
+        res.render("users/register", { messageError: "The user already exists" });
+        return;
+      }
+
+      let salt = bcrypt.genSaltSync(saltRounds);
+      let encryptedPassword = bcrypt.hashSync(password, salt);
+
+      User.create({
+        username: username,
+        password: encryptedPassword
+      })
+        .then(result => {
+          res.redirect("/user/login");
+        })
+        .catch(err => next(err))
+    })
+    .catch(err => {
+      console.log("err ", err);
+      next(err);
+    })
+
+})
+
+router.get("/login", (req, res, next) => {
+  console.log("REQ.SESSION: ", req.session);
+  res.render("users/login");
+})
+
+router.post("/login",  (req, res, next) => {
+  let { username, password } = req.body;
+  if (username == "" || password == "") {
+    res.render("users/login", { messageError: "Missing fields" });
+    return;
+  }
+
+  User.find({ username })
+    .then(results => {
+      if (results.length == 0) {
+        res.render("users/login", { messageError: "Incorrect Credentials" });
+        return;
+      }
+
+
+      if (bcrypt.compareSync(password, results[0].password)) {
+        req.session.currentUser = username; //en req.session.currentUser guardamos la información del usuario que nos interese. Podemos guardar un string o un objeto con todos los datos
+        res.redirect("/user/profile");
+      } else {
+        res.render("users/login", { messageError: "Incorrect Credentials" });
+      }
+    })
+    .catch(err => next(err));
+
+})
+
+router.get("/profile",  (req, res, next) => {
+  res.render("users/profile", { username: req.session.currentUser });
+})
+
+router.get("/logout",  (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    else res.redirect("/user/login");
+  });
+});
+
+module.exports = router;
